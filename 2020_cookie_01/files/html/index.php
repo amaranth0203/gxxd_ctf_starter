21e5bb533ded4b22534d0d76bad9b50755a70b45
qiyunhu <yunhuqi@gmail.com>
2020-07-03 21:13:46 -0400

    添加题目:cookie_01

---
+++ 2020_cookie_01/files/html/index.php
@@ -0,0 +1,28 @@
+<?php
+ob_start();
+?>
+
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>cookie1</title>
+  <style>
+    body {
+      margin-left: auto;
+      margin-right: auto;
+      margin-TOP: 200PX;
+      width: 20em;
+    }
+  </style>
+</head>
+
+<body>
+  <h3>你知道如何查看cookie不?</h3>
+</body>
+
+</html>
+
+<?php
+
+
