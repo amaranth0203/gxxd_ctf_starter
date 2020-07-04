2ba3bbb46bebcff284575be772ecadd20991721f
戚云琥 <qiyunhu@qiyunhudeMacBook-Pro.local>
2020-07-05 01:03:28 +0800

    添加题目:万能密码

--- 2020_sqli_wnmm/files/flag.sh
+++ 2020_sqli_wnmm/files/flag.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # 修改数据库中的 FLAG
-mysql -e "CREATE DATABASE IF NOT EXISTS gxxd;USE gxxd; CREATE TABLE IF NOT EXISTS \`chengji\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`chengji\` VALUES ('$FLAG');" -uroot -proot
+# mysql -e "CREATE DATABASE IF NOT EXISTS gxxd;USE gxxd; CREATE TABLE IF NOT EXISTS \`chengji\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`chengji\` VALUES ('$FLAG');" -uroot -proot
 
+echo "<?php \$flag=\"$FLAG\";?>" >>/var/www/html/config.php
 export FLAG=not_flag
 FLAG=not_flag
 
--- 2020_sqli_wnmm/files/html/index.php
+++ 2020_sqli_wnmm/files/html/index.php
@@ -2,38 +2,53 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>union select</title>
+    <title>万能密码</title>
 </head>
 
 <body>
-  <h1>sqlmap是没有灵魂的</h1>
-<form method="get">
-    姿势: <input type="text" name="inject" value="1">
-    <input type="submit">
-</form>
+  <h1>
+    万能密码属于sql注入的一种<br />
+    不过又略有不同<br />
+    你能说出相同点和不同点不<br />
+  </h1>
+    <form method="post">
+        <div class="input-group">
+            <input style="width:280px;" id="username" type="text" class="form-control" placeholder="用户名" aria-describedby="basic-addon1" name="username">
+        </div>
+        <br>
+        <br>
+        <div class="input-group">
+            <input style="width:280px;" id="password" type="password" class="form-control" placeholder="密码" aria-describedby="basic-addon1" name="password">
+        </div>
+        <br>
+        <br>
+        <button style="width:280px;" class="btn btn-default">登录</button>
+        <br>
+        <br>
+        <button style="width:280px;" class="btn btn-default" type="reset">重置</button>
+    </form>
 
-<pre>
+    <pre>
 <?php
-if(isset($_GET['inject'])) {
-    $id = $_GET['inject'];
-    $mysqli = new mysqli("127.0.0.1","root","root","gxxd");
+include("config.php");
+if (isset($_POST['username'])) {
+    $id = $_POST['username'];
+    $mysqli = new mysqli("127.0.0.1", "root", "root", "gxxd");
     //多条sql语句
     $sql = "select * from `words` where id = '$id';";
-    echo "当前执行语句：".$sql."<br><hr>";
+    echo "当前执行语句：" . $sql . "<br><hr>";
     if (!$result = $mysqli->query($sql)) {
         echo "Errno: " . $mysqli->errno . "\n";
         echo "Error: " . $mysqli->error . "\n";
         exit;
     }
     if ($result->num_rows === 0) {
-        echo "We could not find a match for ID $id, sorry about that. Please try again.";
+        echo "用户名或密码错误";
         exit;
-    }
-    else {
+    } else {
         //echo "var_dump(\$mysqli->query(\$sql)->fetch_assoc()):<br><hr>";
         //var_dump($result->fetch_assoc());
-        echo "找到了一些记录....但我不告诉你是什么"
-        echo "<br>";
+        echo $flag;
     }
 
     $result->free();
@@ -42,7 +57,6 @@ if(isset($_GET['inject'])) {
 
 
 ?>
-</pre>
 
 </body>
 
