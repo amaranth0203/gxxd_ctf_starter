a69311c17b51113a0f4679c48bb0f311c3dcc11d
qiyunhu <yunhuqi@gmail.com>
2020-07-03 21:32:53 -0400

    添加题目:check_in_04

---
+++ 2020_check_in_04/files/flag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "" >> /var/www/html/index.php
+echo "header(\"f1ag: $FLAG\");" >> /var/www/html/index.php
+echo "ob_end_flush();" >> /var/www/html/index.php
+echo "?>" >> /var/www/html/index.php
+echo "" >> /var/www/html/index.php
+echo "<!-- flag 还是不在这里 -->" >>/var/www/html/index.php
+
+export FLAG=not_flag
+FLAG=not_flag
+
+rm -f /flag.sh
---
+++ 2020_check_in_04/files/html/index.php
@@ -0,0 +1,28 @@
+<?php
+ob_start();
+?>
+
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>签到题4</title>
+  <style>
+    body {
+      margin-left: auto;
+      margin-right: auto;
+      margin-TOP: 200PX;
+      width: 20em;
+    }
+  </style>
+</head>
+
+<body>
+  <h3>这次我不讲故事了,flag已经告诉你了,你慢慢找找吧</h3>
+</body>
+
+</html>
+
+<?php
+
+
